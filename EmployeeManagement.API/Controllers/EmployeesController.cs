9865a2bd72be340af840e1dbaa0af980d2e14b75
RjDevOnrun <john.roby76@gmail.com>
2021-07-24 22:58:10 +0900

    Add project files.

---
+++ EmployeeManagement.API/Controllers/EmployeesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EmployeeManagement.API.Models;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeesController(IEmployeeRepository employeeRepository)
+        {
+            this._employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetEmployees()
+        {
+            try
+            {
+                return Ok(await _employeeRepository.GetEmployees());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ EmployeeManagement.API/Models/EmployeeRepository.cs
@@ -0,0 +1,69 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.API.Models
+{
+    public class EmployeeRepository
+        :IEmployeeRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public EmployeeRepository(AppDbContext appDbContext)
+        {
+            this._appDbContext = appDbContext;
+        }
+
+        public async Task<Employee> AddEmployee(Employee employee)
+        {
+            var result = await this._appDbContext.Employees.AddAsync(employee);
+            await _appDbContext.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public async void DeleteEmployee(int employeeId)
+        {
+            var result = await _appDbContext.Employees
+                .FirstOrDefaultAsync(e => e.EmployeeId == employeeId);
+            if(result != null)
+            {
+                _appDbContext.Employees.Remove(result);
+                await _appDbContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task<Employee> GetEmployee(int employeeId)
+        {
+            return await _appDbContext.Employees
+                .FirstOrDefaultAsync(e => e.EmployeeId == employeeId);
+        }
+
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            return await this._appDbContext.Employees.ToListAsync();
+        }
+
+        public async Task<Employee> UpdateEmployee(Employee employee)
+        {
+            var result = await _appDbContext.Employees
+                .FirstOrDefaultAsync(e => e.EmployeeId == employee.EmployeeId);
+            if (result != null)
+            {
+                result.FirstName = employee.FirstName;
+                result.LastName = employee.LastName;
+                result.Email = employee.Email;
+                result.DateOfBirth = employee.DateOfBirth;
+                result.Gender = employee.Gender;
+                result.DepartmentId = employee.DepartmentId;
+                result.PhotoPath = employee.PhotoPath;
+
+                await _appDbContext.SaveChangesAsync();
+                return result;
+            }
+            return null;
+        }
+    }
+}
